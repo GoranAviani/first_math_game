df450a87d66adafa0cf4074047a9bcae6491792a
goranaviani <goranaviani@gmail.com>
2020-08-30 13:11:33 +0200

    displaying menu

--- main.py
+++ main.py
@@ -8,10 +8,15 @@ def display_intro():
     print(game_title)
     print("*" * len(game_title))
 
+def display_menu():
+    menu_list = ["Press N for [N]ew game", "Press X for E[x]it"]
+    for menu_option in menu_list:
+        print(menu_option)
+
 
 def main():
     display_intro()
-
+    display_menu()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
