669f8c2aed0eea1fed138a7829c5681f891ae73e
goranaviani <goranaviani@gmail.com>
2020-08-30 13:18:17 +0200

    first skeleton of the game

--- main.py
+++ main.py
@@ -13,10 +13,25 @@ def display_menu():
     for menu_option in menu_list:
         print(menu_option)
 
+def display_separator():
+    print("*" * 30)
+
+def get_user_input():
+    user_input = input("What is your selection?: ")
+    return user_input.lower()
+
+def play_game():
+    print("This is a game")
 
 def main():
     display_intro()
     display_menu()
+    user_input = get_user_input()
+    if user_input == "n":
+        display_separator()
+        play_game()
+    else:
+        exit()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
